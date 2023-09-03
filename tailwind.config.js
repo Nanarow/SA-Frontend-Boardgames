57c052b81ef5f169f5745f0bdcdbbc3646086cd6
woranan sholee <woranan.sholee@gmail.com>
2023-09-03 23:50:00 +0700

    path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/components/*.{ts,tsx}",
+    "./src/pages/*.{ts,tsx}",
   ],
   theme: {
     extend: {
