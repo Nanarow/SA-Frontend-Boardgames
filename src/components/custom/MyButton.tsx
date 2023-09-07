eabdf3477fc7ec6b5b7e5c34f571647b869c1a37
woranan sholee <woranan.sholee@gmail.com>
2023-09-07 22:04:40 +0700

    add for

--- src/components/custom/MyButton.tsx
+++ src/components/custom/MyButton.tsx
@@ -6,28 +6,31 @@ interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
     bgColor?: string;
     addClass?: string;
 }
-const MyButton: React.FC<IButton> = ({
-    label,
-    leftIcon,
-    bgColor = 'bg-white',
-    addClass = '',
-    ...buttonProps
-}) => {
-    return (
-        <button
-            className={`btn ${bgColor} ${addClass}`}
-            {...buttonProps}
-        >
-            <div className="flex flex-row items-center justify-center space-x-1">
-                {leftIcon && (
-                    <div className="h-[24px] flex items-center">
-                        {leftIcon}
-                    </div>
-                )}
-                {label && <label className="cursor-pointer">{label}</label>}
-            </div>
-        </button>
-    )
-}
+const MyButton = React.forwardRef<HTMLButtonElement, IButton>(
+    ({
+        label,
+        leftIcon,
+        bgColor = 'bg-white',
+        addClass = '',
+        ...buttonProps
+    }, ref) => {
+        return (
+            <button
+                className={`btn ${bgColor} ${addClass}`}
+                {...buttonProps}
+                ref={ref}
+            >
+                <div className="flex flex-row items-center justify-center space-x-1">
+                    {leftIcon && (
+                        <div className="h-[24px] flex items-center">
+                            {leftIcon}
+                        </div>
+                    )}
+                    {label && <label className="cursor-pointer">{label}</label>}
+                </div>
+            </button>
+        )
+    }
+)
 
 export default MyButton
